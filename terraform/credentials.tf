b3a7858bc3952962b7b08eac81f417e871ec5edc
RAFASOLIS <RAFASOLIS1984@GMAIL.COM>
2022-07-22 13:35:42 +0200

    Update credentials.tf

--- terraform/credentials.tf
+++ terraform/credentials.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   features {}
-  subscription_id = "<SUBSCRIPCION ID>"
+  subscription_id = "1682e378-9517-48fd-8e93-893424022161"
   client_id       = "<APP_ID>"
   client_secret   = "<PASSWORD>"
   tenant_id       = "<TENANT>"
