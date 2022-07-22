096e3b49ff2215fc2c76c82a94a0f2be3df7c716
RAFASOLIS <RAFASOLIS1984@GMAIL.COM>
2022-07-22 14:24:20 +0200

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -7,5 +7,5 @@ variable "location" {
 variable "vm_size" {
   type = string
   description = "Tamaño de la máquina virtual"
-  default = "Standard_D1_v2" # 4.0 GB, 2 vCPU 
+  default = "Standard_D1_v2" # 3.5 GB, 1 vCPU 
 }
