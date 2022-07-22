f057b2ca6edcea88750344feca9868d7a7948af3
RAFASOLIS <RAFASOLIS1984@GMAIL.COM>
2022-07-22 14:50:19 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ terraform {
 # crea un service principal y rellena los siguientes datos para autenticar
 provider "azurerm" {
   features {}
-  subscription_id = "<SUBSCRIPCION ID>"
+  subscription_id = "1682e378-9517-48fd-8e93-893424022161"
   client_id       = "<APP_ID>"
   client_secret   = "<PASSWORD>"
   tenant_id       = "<TENANT>"
